67172c1a5830529c9dee0173b55b127171ed28bb
m-mouchard <manueldiezdeonate@hotmail.com>
2024-03-19 18:00:24 +0100

    (Completo)Lista de tareas guardando en local storage

--- 10matricesyarreglos.js
+++ 10matricesyarreglos.js
@@ -17,46 +17,36 @@ for (let i = 0; i < nombres.length; i++) {
 window.onload = iniciar();
 
 let tarea = [];
-function iniciar(){
 
+function iniciar(){
     let boton = document.getElementById("btnAgregar");
     boton.addEventListener("click",logicaBtn);
-    
 }
-
 
 function logicaBtn(){
-
-    let nuevaTarea = [];
-    let variablesLocal = [];
-    
-    nuevaTarea = document.getElementById("newTarea").value;
-    tarea.push(nuevaTarea);
-    console.log(tarea);
-
-    variablesLocal.push = localStorage.setItem("Lista Tareas", nuevaTarea);
-
+    tarea.push(document.getElementById("newTarea").value);
+    //console.log(tarea);
     //aquí llamamos a la función mostrarTareas para que las vaya escribiendo en la web, en el párrafor con el id = "lista"
-    mostrarTareas(variablesLocal);
-
+    mostrarTareas();
 }
 
-function mostrarTareas(variablesLocal){
-
-    var listado = document.getElementById("lista") //Cogemos el párrafo con el id = "lista"
-
-    nuevoCodigo = []; //inicializamos para que no de error al rellenar esta variable en el bucle
-
+function mostrarTareas(){   
+    let listado = document.getElementById("lista") //Cogemos el párrafo con el id = "lista"
+    let var1 =[];
+    let var2;
+    nuevoCodigo = ""; //inicializamos para que no de error al rellenar esta variable en el bucle
+    
     //Manera de usar el bucle for para que recorra todo el vector. 
     //La variable "tarea" existe de la función donde esta a su vez se está invocando. 
     for(let x of tarea){
         //nuevoCodigo = nuevoCodigo + x + "<br/>"; //le metemos salto de linea en html. Otra opción 
         nuevoCodigo += x + "<br/>"; //opción más breve
     }
+    var1 = JSON.stringify(tarea);
+    var2 = localStorage.setItem("Lista Tareas", var1);
     
     listado.innerHTML = nuevoCodigo; //finalmente insertamos nuevo código html en el elementro listado que recogía el parrafo 
     //con el id "lista" y le metemos la variable nuevoCodigo que hemos rellenado en el for con las tareas. 
-    
 }
 
 
