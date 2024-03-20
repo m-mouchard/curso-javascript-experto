d1bbe4736aee5e16bf50ff0863116690b7759c8c
m-mouchard <manueldiezdeonate@hotmail.com>
2024-03-20 18:52:22 +0100

    lista de tareas guardadas en localstorage

--- 10Amatricesyarreglos.js
+++ 10Amatricesyarreglos.js
@@ -1,6 +1,5 @@
-let listaTareas=[];
-
 window.onload = iniciar();
+let listaTareas=[];
 
 function iniciar(){
     let btonAgregar = document.getElementById("btnAgregar");
@@ -15,6 +14,11 @@ function clickBtonAgregar(){
     let txt = document.getElementById("newTarea").value;
     listaTareas.push(txt);
     //localStorage.setItem("Lista de tareas", JSON.stringify(listaTareas));
+    //Como listaTareas es un objeto y no podemos guardar estos en el local storage,
+    //lo convertimos a string con la función JSON.stringy
+    //tareasStorage = localStorage.setItem("Lista de tareas",JSON.stringify(listaTareas));
+    localStorage.tareasStorage = JSON.stringify(listaTareas);
+    //console.log(JSON.parse(localStorage.tareasStorage));
     mostrarTareas();
 }
 
@@ -22,14 +26,16 @@ function mostrarTareas(){
     let para = document.getElementById("lista");
     let htmlString = "";
     
-    for (let i = 0; i < listaTareas.length; i++) {
-        const element = listaTareas[i];
-        htmlString += "Tarea "+ (i+1)+": " +listaTareas[i]+"<br/>";
-        debugger;
+    // Verificar si localStorage.tareasStorage está definido y no es undefined
+    if (localStorage.tareasStorage!= undefined) {
+        let listaTareas = JSON.parse(localStorage.tareasStorage);
+        for (let i = 0; i < listaTareas.length; i++) {
+            htmlString += "Tarea " + (i + 1) + ": " + listaTareas[i] + "<br/>";
+        }
+    } else {
+        htmlString = "No hay tareas almacenadas";
     }
     
-    htmlString = htmlString;
-
     //localStorage.setItem("Lista Tareas post bucle",JSON.stringify(htmlString));
     
     //let htmlFinalParse = JSON.stringify(htmlString);
